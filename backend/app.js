2894802ea5e7f7ad81d127b25125ac84e3a8aadd
tristan <thuckabee.work@gmail.com>
2022-03-31 12:25:15 -0500

    Phase 2 Complete: Error Handling

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 
 const { environment } = require('./config');
 const { isProduction } = environment === 'production';
+const { ValidationError } = require('sequelize');
 
 const app = express();
 const routes = require('./routes');
@@ -31,7 +32,31 @@ app.use(
       }
     })
   );
-  
   app.use( routes );
+  app.use( (_req, _res, next) => {
+    const err = new Error('The requested resource couldn\'t be found.');
+    err.title = 'Resource Not Found';
+    err.errors = ['The requested resource couldn\'t be found.'];
+    err.status = 404;
+    next(err);
+  });
+  app.use( (err, _req, _res, next) => {
+    if ( err instanceof ValidationError ) {
+      err.errors = err.errors.map( (e) => e.message);
+      err.title = 'Validation Error';
+    }
+    next(err);
+  });
+  app.use( (err, _req, res, _next) => {
+    res.status( err.status || 500 );
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
+
     
 module.exports = app;
