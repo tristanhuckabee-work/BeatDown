b2f7832ab153cca8c65f4d4fb1a2988d6cc4a2d9
tristan <thuckabee.work@gmail.com>
2022-03-31 15:30:25 -0500

    handleValidationErrors

---
+++ backend/utils/validation.js
@@ -0,0 +1,20 @@
+const { validationResult } = require('express-validator');
+
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
+
+  if ( !validationErrors.isEmpty() ) {
+    const errors = validationErrors.array().map( (err) => `${err.msg}`);
+
+    const err = new Error('Bad Request.');
+    err.errors = errors;
+    err.status = 400;
+    err.title = 'Bad Request';
+    next(err);
+  }
+  next();
+};
+
+module.exports = {
+  handleValidationErrors
+};
