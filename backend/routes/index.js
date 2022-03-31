f3339843e138b9125fcfd0f0e8423c79256bb10b
tristan <thuckabee.work@gmail.com>
2022-03-31 16:46:24 -0500

    Restore XSRF Token Cookie

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,29 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-// router.get('/hello/world', function(req, res) {
-//   res.cookie( 'XSRF-TOKEN', req.csrfToken() );
-//   res.send('Hello World!')
-// });
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+
+  router.get( '/', (req, res) => {
+    res.cookie( 'XSRF-TOKEN', req.csrfToken() );
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  router.use( express.static( path.resolve('../frontend/build' ) ) );
+
+  router.get( /^(?!\/?api).*/, (req, res) => {
+    res.cookie( 'XSRF-TOKEN', req.csrfToken() );
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+} else {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken() );
+    return res.json();
+  });
+}
 
 module.exports = router;
