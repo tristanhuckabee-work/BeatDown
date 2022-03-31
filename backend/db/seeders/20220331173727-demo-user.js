ef937c5341be8c98205a16c0060750114dcd0eb1
tristan <thuckabee.work@gmail.com>
2022-03-31 14:52:10 -0500

    Phase 3 Complete

---
+++ backend/db/seeders/20220331173727-demo-user.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'yungdemo@user.io',
+        username: 'Yung-Demo',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'sepia13@user.io',
+        username: 'VaporHax',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'sjw-queen-slay@user.io',
+        username: 'iRememberTumblr',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'VaporHax', 'iRememberTumblr'] }
+    }, {});
+  }
+};
