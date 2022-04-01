375ea22ba122556dba200fc1957a95b2785bc3eb
tristan <thuckabee.work@gmail.com>
2022-04-01 11:46:26 -0500

    Fixed Spelling Mistakes & Login Thunk Done

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -2,10 +2,10 @@ import Cookies from 'js-cookie';
 
 export async function csrfFetch(url, options = {}) {
   options.method = options.method || 'GET';
-  options.headers = options.header || {};
+  options.headers = options.headers || {};
   if ( options.method.toUpperCase() !== 'GET' ) {
-    options.headers['Content-Type'] = options.headers['Content-Type'] || 'application/json;';
-    options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
+    options.headers['Content-Type'] = options.headers['Content-Type'] || 'application/json';
+    options.headers['XSRF-TOKEN'] = Cookies.get('XSRF-TOKEN');
   }
 
   const res = await window.fetch(url, options);
