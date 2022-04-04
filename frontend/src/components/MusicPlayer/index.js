0f3340630d05812e87d3d5b4e986fe132ce57b0f
tristan <thuckabee.work@gmail.com>
2022-04-04 17:31:24 -0500

    Finished Music Player Function and CSS

--- frontend/src/components/MusicPlayer/index.js
+++ frontend/src/components/MusicPlayer/index.js
@@ -6,101 +6,101 @@ import './musicPlayer.css';
 
 const MusicPlayer = ({ song }) => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [duration, setDuration] = useState(0);
+  const [trackLength, setTrackLength] = useState(0);
   const [currTime, setCurrTime] = useState(0);
-  const [currVol, setCurrVol] = useState(50);
 
-  const musicPlayer = useRef(); //uses to reference audio component
-  const progressBar = useRef(); //references progressBar
-  const slider = useRef(); //reference animation of slider
-  const volRange = useRef(); //reference volume slider
+  const musicPlayer = useRef();
+  const progress = useRef();
+  const knobbyPos = useRef();
 
-  const depend1 = musicPlayer?.current?.loadedmetadata;
-  const depend2 = musicPlayer?.current?.readyState;
+  useEffect( () => {
+    const secs = Math.floor(musicPlayer.current.duration)
+    setTrackLength(secs);
+
+    progress.current.max = secs;
+  }, [musicPlayer?.current?.loadedmetadata, musicPlayer?.current?.readyState]);
 
   const togglePlayPause = () => {
-    setIsPlaying(!isPlaying);
-    const prevValue = isPlaying;
-    setIsPlaying(!prevValue);
-    if (!prevValue) {
+    const prevVal = isPlaying
+    setIsPlaying(!prevVal);
+    if (!prevVal) {
       musicPlayer.current.play();
-      slider.current = requestAnimationFrame(isisPlaying);
+      knobbyPos.current = requestAnimationFrame(playing);
     } else {
       musicPlayer.current.pause();
-      cancelAnimationFrame(slider.current);
+      cancelAnimationFrame(knobbyPos.current);
     }
   };
 
-  useEffect(() => {
-    const seconds = Math.floor(musicPlayer.current.duration);
-    setDuration(seconds);
+  const playing = () => {
+    progress.current.value = musicPlayer.current.currentTime;
+    changeCurrTime();
+    knobbyPos.current = requestAnimationFrame(playing);
+  }
 
-    progressBar.current.max = seconds;
-  }, [depend1, depend2]);
+  const changeScrub = () => {
+    musicPlayer.current.currentTime = progress.current.value;
+    changeCurrTime();
+  }
 
-  const time = (sec) => {
-    const minutes = Math.floor(sec / 60);
-    const returnMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  const calcTime = (secs) => {
+    const minutes = Math.floor( secs / 60 );
+    const seconds = secs % 60;
+    const newMins = minutes < 10 ? `0${minutes}` : minutes;
+    const newSecs = seconds < 10 ? `0${seconds}` : seconds;
 
-    const seconds = Math.floor(sec % 60);
-    const returnSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${newMins}:${newSecs}`;
+  }
 
-    return `${returnMinutes}:${returnSeconds}`;
-  };
+  const changeCurrTime = () => {
+    progress.current.style.setProperty('--seek-before-width', `${progress.current.value / trackLength * 100}%`);
+    setCurrTime(progress.current.value);
+  }
 
-  const isisPlaying = () => {
-    progressBar.current.value = musicPlayer.current.currTime;
-    changemusicPlayerTime();
-    slider.current = requestAnimationFrame(isisPlaying);
-  };
+  const changeThirty = (op) => {
+    if (op === 'minus') {
+      progress.current.value = Number(progress.current.value - 30);
+    } else {
+      progress.current.value = Number(progress.current.value + 30);
+    }
+    changeScrub();
+  }
 
-  const changeRange = () => {
-    musicPlayer.current.currTime = progressBar.current.value;
-    changemusicPlayerTime();
-  };
-
-  const changeVolume = () => {
-    musicPlayer.current.currVol = volRange.current.value;
-    setCurrVol(volRange.current.value);
-  };
-
-  const changemusicPlayerTime = () => {
-    progressBar.current.style.setProperty(
-      "--seek-before-width",
-      `${(progressBar.current.value / duration) * 100}%`
-    );
-    setCurrTime(progressBar.current.value);
-  };
+  //--------------------------------------------------------
 
   return (
     <div className="audioControl">
       <audio ref={musicPlayer} src={song}></audio>
       <div className='scrubControl'>
         <div className="songTimer current-time">
-          {time(currTime)}
+          {
+            calcTime(currTime)
+          }
         </div>
         <div>
           <input
-            className="progress"
             type="range"
+            className="progress"
             defaultValue="0"
-            ref={progressBar}
-            onChange={changeRange}
+            ref={progress}
+            onChange={changeScrub}
           />
         </div>
-        <div className="songTimer duration">
-          {duration && !isNaN(duration) && time(duration)}
+        <div className="songTimer">
+          {
+            ( trackLength && !isNaN(trackLength) ) && calcTime(trackLength)
+          }
         </div>
       </div>
 
       <div className='trackControl'>
-        <button>
+        <button onClick={e => changeThirty('minus')} >
           <i className='fas fa-backward-fast fa-2x'></i>
         </button>
         <button onClick={togglePlayPause}>
-        { isisPlaying ? <i className='fas fa-circle-pause fa-3x'></i> : <i className='fas fa-circle-play fa-3x'></i>}
+        { isPlaying ? <i className='fas fa-circle-pause fa-3x'></i> : <i className='fas fa-circle-play fa-3x'></i>}
         </button>
-        <button>
+        <button onClick={e => changeThirty('plus')} >
           <i className='fas fa-forward-fast fa-2x'></i>
         </button>
       </div>
