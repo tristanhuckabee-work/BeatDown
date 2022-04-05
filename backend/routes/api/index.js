3578d8b02680309a2b0e5542e31dd893aac54837
tristan <thuckabee.work@gmail.com>
2022-04-05 10:53:21 -0500

    made routes to get music

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,13 +1,10 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const songRouter = require('./search.js');
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
-
-//TEST EXISTENCE
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
+router.use('/search', songRouter);
 
 module.exports = router;
